aa5f0942e9bc55d9ac5b567c14c758bc8a6f3fbe
OnionPatchTv <21-02866@g.batstate-u.edu.ph>
2025-04-18 11:23:07 +0800

    MInor Error  fixes

--- app/my-skills/page.tsx
+++ app/my-skills/page.tsx
@@ -1,31 +1,31 @@
 "use client"
 
-import 'swiper/css'
-import React from 'react'
-import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { FrontAndBackEndSkilLData, UiUxGraphicsSkillData} from '@/constants'
-import { Autoplay } from 'swiper/modules'
+import "swiper/css"
+import React from "react"
+import Image from "next/image"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { FrontAndBackEndSkilLData, UiUxGraphicsSkillData} from "@/constants"
+import { Autoplay } from "swiper/modules"
 
 const Page = () => {
   return (
     <div
       style={{ backgroundImage: "url(/city-bg-1.jpg" }}
-      className='h-screen w-screen flex flex-col items-center justify-center bg-cover bg-center'>
-      <div className='flex flex-col gap-20 max-w-[80%] text-center items-center mx-auto'>
-        <div className='flex flex-col items-center gap-4'>
-          <h1 className='font-semibold text-white text-[50px]'> Skills
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'>
+      className="h-screen w-screen flex flex-col items-center justify-center bg-cover bg-center">
+      <div className="flex flex-col gap-20 max-w-[80%] text-center items-center mx-auto">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="font-semibold text-white text-[50px]"> Skills
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
               & {" "}
             </span> Technologies </h1>
-          <p className='text-white-400 text-[20px]'>
-          This portfolio demonstrates a foundational skill set in full-stack development, encompassing front-end technologies like React and Next.js, robust back-end systems built with C# and Java, and database management using Postgres. Additionally, it showcases my capabilities in UI/UX design and graphic design. As a recent graduate, I'm eager to expand upon these skills and contribute to innovative projects.          </p>
+          <p className="text-white-400 text-[20px]">
+          This portfolio demonstrates a foundational skill set in full-stack development, encompassing front-end technologies like React and Next.js, robust back-end systems built with C# and Java, and database management using Postgres. Additionally, it showcases my capabilities in UI/UX design and graphic design. As a recent graduate, I"m eager to expand upon these skills and contribute to innovative projects.          </p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-2 p-2">
           <div>
-            <h5 className='font-semibold text-white text-[50px]'> Front-End
-              <span className='text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-purple-500'>
+            <h5 className="font-semibold text-white text-[50px]"> Front-End
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-purple-500">
                 {" "}
                 & {" "}
               </span> Backend </h5>
@@ -38,7 +38,7 @@ const Page = () => {
               }}
               speed={5000}
               modules={[Autoplay]}
-              className='max-w-[80%]'>
+              className="max-w-[80%]">
               {FrontAndBackEndSkilLData.map((skill, index) => (
                 <SwiperSlide key={index}>
                   <Image src={skill.Image}
@@ -59,7 +59,7 @@ const Page = () => {
               }}
               speed={5000}
               modules={[Autoplay]}
-              className='max-w-[80%]'>
+              className="max-w-[80%]">
               {FrontAndBackEndSkilLData.map((skill, index) => (
                 <SwiperSlide key={index}>
                   <Image src={skill.Image}
@@ -72,8 +72,8 @@ const Page = () => {
             </Swiper>
           </div>
           <div>
-            <h5 className='font-semibold text-white text-[50px]'> UI/UX
-              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'>
+            <h5 className="font-semibold text-white text-[50px]"> UI/UX
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
                 {" "}
                 & {" "}
               </span> Video Editing </h5>
@@ -86,7 +86,7 @@ const Page = () => {
               }}
               speed={5000}
               modules={[Autoplay]}
-              className='max-w-[80%]'>
+              className="max-w-[80%]">
               {UiUxGraphicsSkillData.map((skill, index) => (
                 <SwiperSlide key={index}>
                   <Image src={skill.Image}
@@ -107,7 +107,7 @@ const Page = () => {
               }}
               speed={5000}
               modules={[Autoplay]}
-              className='max-w-[80%]'>
+              className="max-w-[80%]">
               {UiUxGraphicsSkillData.map((skill, index) => (
                 <SwiperSlide key={index}>
                   <Image src={skill.Image}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client"
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import Typewriter from '../components/TypeWriter';
-import ContactModal from '../components/ContactModal';
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Typewriter from "../components/TypeWriter";
+import ContactModal from "../components/ContactModal";
 
 export default function Home() {
   const [isContactModalOpen, setContactModalOpen] = useState(false);
@@ -17,55 +17,55 @@ export default function Home() {
   };
 
   useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    document.documentElement.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
+    document.documentElement.style.overflow = "hidden";
 
     return () => {
-      document.body.style.overflow = '';
-      document.documentElement.style.overflow = '';
+      document.body.style.overflow = "";
+      document.documentElement.style.overflow = "";
     };
   }, []);
 
   return (
-    <main className='w-screen h-screen relative overflow-hidden'>
-      <div className='flex items-center w-full h-full bg-cover bg-center' 
+    <main className="w-screen h-screen relative overflow-hidden">
+      <div className="flex items-center w-full h-full bg-cover bg-center" 
         style={{ 
           backgroundImage: "url('/city-of-stars-bg.jpg')", 
           backgroundPositionY: "bottom"
         }}>
         <div className="grid grid-cols-1 md:grid-cols-2 bg-black/50 w-full">
-          <div className='px-6 md:px-10 lg:pl-40 pb-56 pt-10 md:pt-20 md:pb-20 flex flex-col gap-6 z-[10] max-w-[900px]'>
+          <div className="px-6 md:px-10 lg:pl-40 pb-56 pt-10 md:pt-20 md:pb-20 flex flex-col gap-6 z-[10] max-w-[900px]">
             <div className="scale-110 origin-left">
               <Typewriter />
             </div>
 
-            <p className='text-gray-200 text-lg md:text-xl hidden md:block max-w-[95%]'>
-              Hi there! I'm excited to share my work with you. I'm a developer and graphics enthusiast, passionate about building engaging web experiences and creating visually appealing designs. I enjoy tackling both front-end and back-end challenges, and I'm always eager to learn and grow.
+            <p className="text-gray-200 text-lg md:text-xl hidden md:block max-w-[95%]">
+              Hi there! I"m excited to share my work with you. I"m a developer and graphics enthusiast, passionate about building engaging web experiences and creating visually appealing designs. I enjoy tackling both front-end and back-end challenges, and I"m always eager to learn and grow.
             </p>
 
-            <div className='flex-col md:flex-row hidden md:flex gap-5 mt-4'>
-              <Link href='/my-skills' className='rounded-[20px] group relative bg-purple-500 hover:bg-blue-400 px-6 border border-white py-4 text-xl text-white max-w-[220px] text-center'>
+            <div className="flex-col md:flex-row hidden md:flex gap-5 mt-4">
+              <Link href="/my-skills" className="rounded-[20px] group relative bg-purple-500 hover:bg-blue-400 px-6 border border-white py-4 text-xl text-white max-w-[220px] text-center">
                 Learn More
               </Link>
-              <Link href='/my-projects' className='rounded-[20px] group relative bg-transparent hover:bg-blue-400 px-6 border border-white py-4 text-xl text-white max-w-[220px] text-center'>
-                <div className='absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20' />
+              <Link href="/my-projects" className="rounded-[20px] group relative bg-transparent hover:bg-blue-400 px-6 border border-white py-4 text-xl text-white max-w-[220px] text-center">
+                <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
                 My Projects
               </Link>
               <button 
                 onClick={openContactModal}
-                className='rounded-[20px] group relative bg-transparent hover:bg-blue-400 px-6 border border-white py-4 text-xl text-white max-w-[220px] text-center'
+                className="rounded-[20px] group relative bg-transparent hover:bg-blue-400 px-6 border border-white py-4 text-xl text-white max-w-[220px] text-center"
               >
-                <div className='absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20' />
+                <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
                 Contact Me  
               </button>
             </div>
           </div>
 
-          <div className='hidden md:flex items-center justify-center z-[10]'>
-            <div className='relative scale-110 animate-float'>
-              <div className='w-96 h-96 rounded-full border-4 border-white/70 flex items-center justify-center overflow-hidden relative'>
-                <div className='absolute w-full h-full bg-blue-500/30 blur-md'></div>
-                <div className='rounded-full overflow-hidden w-88 h-88 relative'>
+          <div className="hidden md:flex items-center justify-center z-[10]">
+            <div className="relative scale-110 animate-float">
+              <div className="w-96 h-96 rounded-full border-4 border-white/70 flex items-center justify-center overflow-hidden relative">
+                <div className="absolute w-full h-full bg-blue-500/30 blur-md"></div>
+                <div className="rounded-full overflow-hidden w-88 h-88 relative">
                   <Image
                     src="/Profile.jpg"
                     alt="Profile"
@@ -76,22 +76,22 @@ export default function Home() {
                   />
                 </div>
               </div>
-              <div className='absolute -top-5 -left-5 w-10 h-10 bg-blue-500 rounded-full blur-sm'></div>
-              <div className='absolute -bottom-3 -right-3 w-8 h-8 bg-purple-500 rounded-full blur-sm'></div>
+              <div className="absolute -top-5 -left-5 w-10 h-10 bg-blue-500 rounded-full blur-sm"></div>
+              <div className="absolute -bottom-3 -right-3 w-8 h-8 bg-purple-500 rounded-full blur-sm"></div>
             </div>
           </div>
         </div>
 
-        <div className='absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5'>
-          <Link href='/my-skills' className='rounded-[20px] group relative bg-purple-500 px-5 border border-white py-3 text-lg text-white max-w-[200px]'>
+        <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
+          <Link href="/my-skills" className="rounded-[20px] group relative bg-purple-500 px-5 border border-white py-3 text-lg text-white max-w-[200px]">
             Learn More
           </Link>
-          <Link href='/my-projects' className='rounded-[20px] group relative bg-transparent px-5 border border-white py-3 text-lg text-white max-w-[200px]'>
+          <Link href="/my-projects" className="rounded-[20px] group relative bg-transparent px-5 border border-white py-3 text-lg text-white max-w-[200px]">
             My Projects
           </Link>
           <button 
             onClick={openContactModal}
-            className='rounded-[20px] group relative bg-transparent px-5 border border-white py-3 text-lg text-white max-w-[200px]'
+            className="rounded-[20px] group relative bg-transparent px-5 border border-white py-3 text-lg text-white max-w-[200px]"
           >
             Contact Me
           </button>
